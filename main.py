1b71f416591a5a0d97055837c32bdd9c58388983
kai-dg <km.commissions@gmail.com>
2019-05-12 01:20:21 +0000

    added date difference function

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 """Entry point for pub-scraping"""
-from sys import argv
+from datetime import date
+from datetime import datetime
+import re
 from scrapers.base_parse import BaseParse
 from scrapers.meta_scraper import MetaScraper
+from sys import argv
 from wayback import Wayback
 
+
+def find_date(res):
+    """Finds date difference from result to now
+
+    Args:
+        res(str): result from find_pub()
+    """
+    curr = datetime.now().strftime("%Y-%m-%d").split("-")
+    res_d = re.search(r"(\d+-\d+-\d+)", str(res)).group(1).split("-")
+
+    d0 = date(int(curr[0]), int(curr[1]), int(curr[2]))
+    d1 = date(int(res_d[0]), int(res_d[1]), int(res_d[2]))
+
+    delta = d0 - d1
+
+    return "This site is {} days old.\n".format(str(delta.days))
 
 def find_pub():
     """Finds the publication date of the first argument
@@ -23,17 +42,21 @@ def find_pub():
     if wayback.wayback_time:
         print("\n======== Wayback =======")
         print(wayback.wayback_time)
-        print("========================\n")
+        print("========================")
+        print(find_date(wayback.wayback_time))
     else:
         print("\n========= Meta =========")
         nd_counter = 0
         for i in meta_list:
             if i is not None:
                 nd_counter += 1
+                temp = i
                 print(i)
+                break
         if nd_counter == 0:
             print("Metadata: n.d. (No Date)")
-        print("========================\n")
+        print("========================")
+        print(find_date(temp))
 
 if __name__ == "__main__":
     find_pub()
